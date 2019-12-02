c2d37bec5593f0c2f73c277c5a5853890e0f08c2
DinaGenena <dina.m.genena@gmail.com>
2019-12-02 11:58:23 +0100

    Creating Spring boot project

---
+++ src/main/java/com/education/PbApi/PbApiApplication.java
@@ -0,0 +1,13 @@
+package com.education.PbApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbApiApplication.class, args);
+	}
+
+}
